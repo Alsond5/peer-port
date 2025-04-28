5af05653ed0fb89c8adf461da85b6c64497c8a4c
Alsond5 <ahmet_idris_0109@hotmail.com>
2025-04-28 17:20:08 +0300

    Change adapter from static to netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,13 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter({
+			edge: true
+		})
+	}
 };
 
 export default config;
