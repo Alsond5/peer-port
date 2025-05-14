52b1daa2aeb78d49c1fc090fc39f5b0c2fc82d6f
Alsond5 <ahmet_idris_0109@hotmail.com>
2025-05-14 11:00:10 +0300

    fix: fix some errors

--- src/lib/components/Feature.svelte
+++ src/lib/components/Feature.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
-    import bg_shape from "$lib/assets/background_shape.svg?url";
-
     interface Props {
-        position: 'left' | 'right';
+        position: "left" | "right";
         image_url: string;
         title: string;
         text: string;
@@ -18,25 +16,51 @@
         text,
         article1,
         article2,
-        article3
-    }: Props = $props()
-
+        article3,
+    }: Props = $props();
 </script>
 
 <!-- Features -->
 <div class="max-w-[90rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
     <!-- Grid -->
     <div class="md:grid md:grid-cols-2 md:items-center md:gap-12 xl:gap-32">
-        <div class="image relative flex items-center justify-center bg-no-repeat bg-contain bg-center {position === 'right' ? 'md:order-2' : 'md:order-1'} rounded-3xl p-10" style="background-image: url('/src/lib/assets/background_shape.svg');">
+        <div
+            class="image relative flex items-center justify-center bg-no-repeat bg-contain bg-center {position ===
+            'right'
+                ? 'md:order-2'
+                : 'md:order-1'} rounded-3xl p-10"
+        >
+            <div
+                class="absolute w-full h-full flex items-center justify-center"
+            >
+                <svg class="aspect-square max-w-sm" viewBox="0 0 600 800" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M150,150 
+                             C200,50 300,100 350,150 
+                             C450,250 500,300 550,350 
+                             C600,400 550,500 500,550 
+                             C450,600 400,650 350,700 
+                             C300,750 200,700 150,650 
+                             C100,600 50,550 50,450 
+                             C50,350 100,250 150,150 Z"
+                        fill="#dbeafe"
+                        opacity="0.6"
+                    />
+                </svg>
+            </div>
             <img
-                class="max-w-100 w-full h-full aspect-square rounded-xl object-contain"
+                class="max-w-100 w-full h-full aspect-square rounded-xl object-contain z-50"
                 src={image_url}
                 alt="Feature - {title}"
             />
         </div>
         <!-- End Col -->
 
-        <div class="mt-5 {position === 'left' ? 'md:order-2' : 'md:order-1'} sm:mt-10 lg:mt-0">
+        <div
+            class="mt-5 {position === 'left'
+                ? 'md:order-2'
+                : 'md:order-1'} sm:mt-10 lg:mt-0"
+        >
             <div class="space-y-6 sm:space-y-8">
                 <!-- Title -->
                 <div class="space-y-2 md:space-y-4">
@@ -141,6 +165,7 @@
     </div>
     <!-- End Grid -->
 </div>
+
 <!-- End Features -->
 
 <style>
